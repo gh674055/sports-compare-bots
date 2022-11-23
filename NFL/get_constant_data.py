29457e36dfdedead4924bfce0fc5a57de78a4e1f
jharkin22 <53134786+jharkin22@users.noreply.github.com>
2022-11-23 09:53:31 -0500

    Update get_constant_data.py

--- NFL/get_constant_data.py
+++ NFL/get_constant_data.py
@@ -33,7 +33,7 @@ request_headers= {}
 
 start_year = 1936
 end_year = 2022
-skip_current_year = True
+skip_current_year = False
 
 current_year_standings_url = "https://www.pro-football-reference.com/years/{}/games.htm"
 
